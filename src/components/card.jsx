268d61f3952c04ca142ec4fb3f21e1f2a5a677c5
Sneh Vachhani <58119663+SnehVachhani@users.noreply.github.com>
2023-03-20 00:08:50 +0530

    Update card.jsx

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -12,6 +12,9 @@ function Card(props) {
 		}
 	}
 	const {img,name,age,l1,professional,status,hobbies,other} =info[i];
+	if(name==="Sneh Vachhani"){
+		img="https://instagram.fjga1-1.fna.fbcdn.net/v/t51.2885-19/333418284_3553409611611178_1249760614493696429_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fjga1-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=SNdCZcRLVJEAX8Xu2Gs&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfCsBH-0ssWLV8Tc3NrLr_aZdc1ph6PxvMs6pg5EMkQ2TQ&oe=641BD110&_nc_sid=8fd12b"
+	}
 	return (<div>
 			<div className="bout">
 			About:
